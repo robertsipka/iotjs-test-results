46ce9c5eb7d502a3c5829bcbe4bde84a41221fc2
Robert Sipka <rsipka.uszeged@partner.samsung.com>
2019-04-11 09:20:25 +0200

    Add missing dependencies for cross-compiling
    JSRemoteTest-DCO-1.0-Signed-off-by: Robert Sipka rsipka.uszeged@partner.samsung.com

--- install-deps.sh
+++ install-deps.sh
@@ -24,8 +24,8 @@ sudo apt-get install -y autoconf libtool gperf flex bison autoconf2.13
 sudo apt-get install -y cmake libncurses-dev libusb-1.0-0-dev genromfs
 sudo apt-get install -y libsgutils2-dev gcc-arm-none-eabi minicom
 sudo apt-get install -y python-pip pylint pkg-config libssl-dev
+sudo apt-get install -y libc6-armel-cross libc6-dev-armel-cross binutils-arm-linux-gnueabi libncurses5-dev
 sudo apt-get install -y gcc-arm-linux-gnueabihf gcc-arm-linux-gnueabi
-sudo apt-get install -y binutils-arm-linux-gnueabi
 sudo apt-get install -y mosquitto
 sudo apt-get install -y autotools-dev automake
 
